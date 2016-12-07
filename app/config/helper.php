37176fbd2c21bb35d4369341decd51060cbfde50
zhaobing <zhaobing024@gmail.com>
2016-12-07 22:19:04 +0800

    学习laravel的helper撰写方式

--- app/config/helper.php
+++ app/config/helper.php
@@ -5,13 +5,40 @@
  * Date: 2016/12/6
  * Time: 12:11
  */
+use Illuminate\Support\Debug\Dumper;
 
-/**
- * @return \core\myAuth
- */
-function auth(){
-    return \core\myDI::getDefault()->get('auth');
+if(! function_exists('auth')){
+    /**
+     * @return \core\myAuth
+     */
+    function auth(){
+        return \core\myDI::getDefault()->get('auth');
+    }
 }
-function url(array $routeArray){
-    return \core\myDI::getDefault()->get('url')->get($routeArray);
+if(! function_exists('url')){
+    /**
+     * @param array $routeArray
+     * @return string
+     */
+    function url(array $routeArray){
+        return \core\myDI::getDefault()->get('url')->get($routeArray);
+    }
+}
+
+
+if (! function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function dd()
+    {
+        array_map(function ($x) {
+            (new Dumper)->dump($x);
+        }, func_get_args());
+
+        die(1);
+    }
 }
