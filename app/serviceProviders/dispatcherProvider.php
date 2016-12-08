254c2cdcd404eda74deb33e32704c3daeb24b766
zhaobing <zhaobing024@gmail.com>
2016-12-09 06:34:56 +0800

    注释：中间件技术怎么在dispatch过程中的事件机制中获得应用呢？这里可以将这个作为AOP的重要实现方式来做！

--- app/serviceProviders/dispatcherProvider.php
+++ app/serviceProviders/dispatcherProvider.php
@@ -19,12 +19,15 @@ class dispatcherProvider extends myProvider
 
     public function register($name)
     {
+        //todo 中间件技术的重新思考
+        //  这里可以将模型绑定与身份验证以及权限检查等活动都统一用中间件技术来一并提供的，需要重新构建所谓的中间件
+        //  针对不同事情可以在不同的节点去执行，包括类似log的工作应该也是可以在这里来完成的，值得好好研究一下的
 
         $this->di->setShared($name,function(){
             /** @var myDI $this */
             $eventsManager = $this->get('eventsManager');
 
-            $eventsManager->attach("dispatch:beforeDispatch", function(Event $event, Dispatcher $dispatcher){
+            $eventsManager->attach("dispatch:beforeDispatchLoop", function(Event $event, Dispatcher $dispatcher){
                 /** @var myDI $this */
                 //模型注入的功能，这里可以很方便的进行 model binding,这里基本上实现了Laravel中的模型绑定的功能了
                 return $this->get('router')->executeModelBinding($dispatcher);
