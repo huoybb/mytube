fe668b8ff992b38db6316edacc774149d750c161
zhaobing <zhaobing024@gmail.com>
2016-12-25 19:35:23 +0800

    删除debugbar，这个版本安装不了

--- public/index.php
+++ public/index.php
@@ -23,12 +23,6 @@ try {
      */
     $application = new \Phalcon\Mvc\Application($di);
 
-    /**
-     * add phalcon-debugbar
-     */
-//    $di['app'] = $application; //  Important
-//    (new Snowair\Debugbar\ServiceProvider(APP_PATH."/app/config/debugbar.php"))->start();
-
     echo $application->handle()->getContent();
 
 } catch (\Exception $e) {
