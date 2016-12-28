7762faf8904d000c640f12da91b64907a65640ab
zhaobing <zhaobing024@gmail.com>
2016-12-29 07:16:02 +0800

    redis的cache机制研究，更换一种方式

--- app/serviceProviders/viewCacheProvider.php
+++ app/serviceProviders/viewCacheProvider.php
@@ -20,20 +20,19 @@ class viewCacheProvider extends myProvider
     {
         return function(){
             // Cache data for one day by default
+            //@reference https://forum.phalconphp.com/discussion/13699/cache-performance-comparison-file-vs-memcache-vs-apc-vs-mongo-vs
+            //这里有相关的说明，感觉statskey这个东西没有怎么搞明白，为什么需要这个东西呢？估计得看源代码否则比较难理解呀!
             $frontCache = new OutputFrontend(
                 [
                     "lifetime" => 86400,
                 ]
             );
 
-
-            $redis = new \Redis();
-            $redis->connect('127.0.0.1', 6379);
-
             $cache = new Redis($frontCache,[
-                'redis'=>$redis,
+                'host' => '127.0.0.1',
+                'port' => 6379,
+                "statsKey" => 'mytube'
             ]);
-
             return $cache;
         };
     }
